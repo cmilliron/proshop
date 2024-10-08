fc184df675babb7de9d116c7e6bac0eec79aa746
Cody Milliron <codymilliron.dev@gmail.com>
2024-10-08 07:46:20 -0400

    Add Redux and apiSlice.js and productSlice.js.

---
+++ frontend/src/constants.js
@@ -0,0 +1,4 @@
+export const BASE_URL =
+  process.env.NODE_ENV === "development" ? "http//localhost:5500" : "";
+
+export const PRODUCT_URL = "/api/products";
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import HomeScreen from "./screens/HomeScreen";
 
@@ -33,7 +35,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
