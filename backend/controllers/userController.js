da0da78ff4e2f00e1ba2bb8733edb23d3a4dde3e
Cody Milliron <codymilliron.dev@gmail.com>
2024-10-11 16:28:16 -0400

    add serverside api request for authorization. Moved to mongo container for development.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,26 @@
 import asyncHandler from "../middleware/asyncHandler.js";
+import bcrypt from "bcryptjs";
 import User from "../models/userModel.js";
 
 // @desc    Auth user & get token
-// @route   POST /api/users/login
+// @route   POST /api/users/auth
 // @access  Public
 export const authUser = asyncHandler(async (req, res) => {
-  res.send("auth user");
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdimin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc    Register a new user
--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,9 @@ const port = process.env.PORT || 5500;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
