d5807b1572a8df995aace2b150f5c86937976e42
Cody Milliron <codymilliron.dev@gmail.com>
2024-10-10 22:12:04 -0400

    chore: add routes.

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -28,6 +28,10 @@ function CartScreen() {
     dispatch(removeFromCart(id));
   };
 
+  const checkoutHandler = () => {
+    navigate("/login?redirect=/shipping");
+  };
+
   return (
     <Row>
       <Col md={8}>
@@ -97,6 +101,7 @@ function CartScreen() {
                   type="button"
                   className="btn-block"
                   disabled={cartItems.length === 0}
+                  onClick={checkoutHandler}
                 >
                   Proceed To Checkout
                 </Button>
