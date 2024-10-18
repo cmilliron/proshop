c7a3312c2eca48192a455a17b455b1a358e877b8
Cody Milliron <codymilliron.dev@gmail.com>
2024-10-18 01:52:56 +0000

    feat: add login screen and form component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,6 +21,7 @@ import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 // Intiate the Router
 // Set Home Screen as the default Route.
@@ -30,6 +31,7 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/products/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
     </Route>
   )
 );
