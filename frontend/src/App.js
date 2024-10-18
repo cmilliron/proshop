fc016fd3b44eab55d513756c77eac08c9159e78d
Cody Milliron <codymilliron.dev@gmail.com>
2024-10-18 09:20:48 -0400

    feat: add token experiation to front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,27 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "./slices/authSlice";
+
 import { Container } from "react-bootstrap";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const expirationTime = localStorage.getItem("expirationTime");
+    if (expirationTime) {
+      const currentTime = new Date().getTime();
+      if (currentTime > expirationTime) {
+        dispatch(logout());
+      }
+    }
+  }, [dispatch]);
+
   return (
     <>
       <ToastContainer />
